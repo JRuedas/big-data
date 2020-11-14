35ccc2a1c07aa13792dc3e12ec996e88f93e7251
JRuedas <11670560+JRuedas@users.noreply.github.com>
2020-11-14 14:16:01 +0100

    Updates authors and add constants

--- bigdata/src/main/scala/com/upm/muii/App.scala
+++ bigdata/src/main/scala/com/upm/muii/App.scala
@@ -6,24 +6,31 @@ import org.apache.spark.{SparkConf, SparkContext}
 import scala.io.StdIn
 
 /**
- * @author ${user.name}
+ * @author Jose Dominguez Perez
+ * @author Ismael Muñoz Aztout
+ * @author Jonatan Ruedas Mora
  */
 object App {
+
+  val AppName = "Big Data Project"
+  val MasterUrl = "local"
+  val StorageProtocol = "file://"
 
   def configureSpark(): SparkContext = {
 
     val conf = new SparkConf()
-      .setAppName("Big data project")
-      .setMaster("local")
+      .setAppName(AppName)
+      .setMaster(MasterUrl)
 
     new SparkContext(conf)
   }
 
   def loadData(context: SparkContext): RDD[String] = {
 
-    println("Introduce the absolute path to the dataset file" )
-    val filePath = StdIn.readLine()
-    context.textFile("file://" + filePath.trim())
+    println("Introduce the absolute path to the dataset file")
+    val filePath = StdIn.readLine().trim()
+
+    context.textFile(StorageProtocol + filePath)
   }
 
   def main(args : Array[String]) {
