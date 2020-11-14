d7878ce5d09dbb5623f8e8cd737715a3c0ac227f
JRuedas <11670560+JRuedas@users.noreply.github.com>
2020-11-14 13:22:09 +0100

    Adds spark core dependency and loads dataset

--- bigdata/src/main/scala/com/upm/muii/App.scala
+++ bigdata/src/main/scala/com/upm/muii/App.scala
@@ -1,15 +1,23 @@
 package com.upm.muii
 
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.io.StdIn
+
 /**
  * @author ${user.name}
  */
 object App {
-  
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
-  def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
-  }
 
+  def main(args : Array[String]) {
+    println("Introduce the absolute path to the dataset file" )
+    val filePath = StdIn.readLine()
+
+    val conf = new SparkConf().setAppName("Big data project")
+    val context = new SparkContext(conf)
+
+    val myData = context.textFile("file://" + filePath)
+
+    myData.take(10).foreach(println(_))
+  }
 }
